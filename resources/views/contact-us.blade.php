c1fd512b34a5a43e2341b1d1db6dc345180179e6
maidyjoshi <maidyputri13@gmail.com>
2023-03-15 15:46:51 +0700

    first commit

---
+++ resources/views/contact-us.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>Contact Person:
+        0895-6270-54918
+    </h1> 
+</div>
+@endsection
---
+++ resources/views/experience.blade.php
@@ -0,0 +1,20 @@
+@extends('adminlte::page')
+
+@section('title', 'Experience')
+
+@section('content_header')
+<h1>halo</h1>
+@stop
+
+@section('content')
+<p>Saya adalah Mahasiswa Politeknik Negeri Malang</p>
+<h3>Saya sekarang semester 4</h3>
+@stop
+
+@section('css')
+<link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('footer')
+<p class="text-center">Maidy Putri Joshi</p>
+@stop
